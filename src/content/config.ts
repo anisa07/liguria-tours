ec8d57b143b03933f6a80691a84f42055af868e9
anisa07 <askany07@gmail.com>
2025-10-21 23:53:37 +0200

    add tours

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,6 +21,38 @@ const heroCollection = defineCollection({
     }),
 });
 
+const italianToursGallery = defineCollection({
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/content/italianToursGallery",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      themeColor: z.number().min(0).max(360).default(260),
+      tours: z.array(
+        z.object({
+          title: z.string(),
+          slug: z.string(),
+          category: z
+            .enum(["city", "coastal", "cultural", "history"])
+            .default("city"),
+          description: z.string(),
+          fullDescription: z.string(),
+          points: z.array(z.string()),
+          duration: z.string().optional(),
+          price: z.string().optional(),
+          images: z.array(
+            z.object({
+              image: image(),
+              alt: z.string(),
+            })
+          ),
+        })
+      ),
+    }),
+});
+
 export const collections = {
   hero: heroCollection,
+  italianToursGallery,
 };
